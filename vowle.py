70c15899ffc3c1c5d9cd87b5b5dddc79edc54c07
baviamu <48151961+baviamu@users.noreply.github.com>
2019-06-29 21:35:00 +0530

    Create vowle.py

---
+++ vowle.py
@@ -0,0 +1,7 @@
+z=input()
+if z in('a','e','i','o','u','A','E','I','O','U'):
+   print("Vowel")
+elif z in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+    print("Consonent")
+else:
+    print("invalid")

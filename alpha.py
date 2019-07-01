bb0c3b3d84692865def5d2f44336b65c533721b0
baviamu <48151961+baviamu@users.noreply.github.com>
2019-07-01 10:35:41 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,5 +1,5 @@
-st=input("")
-if((st>='a' and st<='z')) or (st>='A' and st<='Z')):
+st = input("Enter a character: ")
+if((st>='a' and st<= 'z') or (st>='A' and st<='Z')):
     print("Alphabet")
 else:
     print("No")
